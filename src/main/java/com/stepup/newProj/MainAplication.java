bbad77213451909ea9baffb366f543ccead8abdd
acer <alukardmasson0@gmail.com>
2025-10-31 09:54:55 +0300

    Доработка логики

--- src/main/java/com/stepup/newProj/MainAplication.java
+++ src/main/java/com/stepup/newProj/MainAplication.java
@@ -3,6 +3,7 @@ package com.stepup.newProj;
 public class MainAplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("111111");
     }
 }
 
