c1bd39b219cea274ce67911ef32d008ecbd8b74d
acer <alukardmasson0@gmail.com>
2025-10-31 10:53:36 +0300

    Изменена основная логика Добовлен класс коробоки

--- src/main/java/com/stepup/newProj/MainAplication.java
+++ src/main/java/com/stepup/newProj/MainAplication.java
@@ -3,7 +3,11 @@ package com.stepup.newProj;
 public class MainAplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("111111");
+        System.out.println("1001");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
     }
 }
 
