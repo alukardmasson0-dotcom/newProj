bb02aea7808ccc57ac0311c9d1fb26519a078ad5
acer <alukardmasson0@gmail.com>
2025-11-01 10:50:03 +0300

    Добавлен вызов метода doSomething()

--- src/main/java/com/stepup/newProj/MainAplication.java
+++ src/main/java/com/stepup/newProj/MainAplication.java
@@ -5,6 +5,7 @@ public class MainAplication {
         System.out.println("///");
         System.out.println("Hello World");
         System.out.println("1001");
+        doSomething();
         System.out.println("---");
     }
 
